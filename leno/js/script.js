bb58c01e512211a2e2948cef749b2b7a127ac1f8
Azim Islam <65490972+Azim-Islam@users.noreply.github.com>
2024-10-21 23:52:22 +0600

    leno final update

--- leno/js/script.js
+++ leno/js/script.js
@@ -5,6 +5,27 @@ document.addEventListener('DOMContentLoaded', function() {
     toggleButton.addEventListener('click', function() {
         mobileMenu.classList.toggle('active')
     })
+
+    const modal = document.getElementById('videoModal');
+    const videoButton = document.querySelector('.preview__video-button');
+    const closeButton = document.querySelector('.modal__close-button');
+    const videoPlr= document.getElementById('videoPlayer');
+
+    videoButton.addEventListener('click', function () {
+        modal.style.display = 'block';
+        videoPlr.src = 'https://www.youtube.com/embed/8sXRyHI3bLw';
+        closeButton.addEventListener('click', function () {
+        modal.style.display = 'none';
+        videoPlr.src = '';
+        });
+        window.addEventListener('click', function (event) {
+        if (event.target == modal) {
+            modal.style.display = 'none';
+            videoPlr.src = '';
+        }
+        });
+    });
+
 })
 
 
@@ -18,3 +39,4 @@ window.addEventListener('scroll', function() {
         navbar.classList.remove('navbar--scroll')
     }
 })
+
